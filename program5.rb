ed8ff09a1c915db37aec4a242f3d72c4c7212f8c
Zenio Angelo <72936093+Zenio2500@users.noreply.github.com>
2023-10-09 14:17:36 -0300

    Add files via upload

---
+++ program5.rb
@@ -0,0 +1,6 @@
+:nome #symbol(serve como uma variável global)
+:nome_completo #uso de underscore/underline
+:"nome completo" #possível usar espaço se usar aspas duplas
+
+"name".to_sym #conversão string->symbol
+:nome.to_s #conversão symbol->string
